d580f58317893206f776ad8de2c827692b30e10a
koushikroy-440 <koushikroy295@gmail.com>
2021-10-10 19:25:43 +0530

    on/off video

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ var peer = new Peer(undefined, {
 let myVideoStream;
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false,
+    audio: true,
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
@@ -67,5 +67,64 @@ $('html').keydown(function (e) {
 
 socket.on('createMessage', message => {
     $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-    // scrollToBottom();
+    scrollToBottom();
 })
+
+//mute our video 
+const muteUnmute = () => {
+    console.log(myVideoStream);
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+
+//stop video playback
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
